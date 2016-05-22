732cd0446da869dae10b93d3c8feb74bc3f1bb51
Nate <nwoods@hep.wisc.edu>
2016-05-22 17:06:31 +0200

    Move final state particle name function to AnalysisTools.

--- AnalysisTools/python/helpers.py
+++ AnalysisTools/python/helpers.py
@@ -21,3 +21,35 @@ def getObjName(obj, capitalize=False):
     if capitalize:
         name = name[0].upper() + name[1:]
     return name
+
+_zzhelpers_object_maps_ = {}
+def mapObjects(channel):
+    '''
+    Return a list of objects of the form ['e1','e2','m1','m2'] or ['e1','e2','m']
+    Objects are in alphabetical/numerical order order
+    '''
+    global _zzhelpers_object_maps_
+
+    try:
+        return _zzhelpers_object_maps_[channel]
+    except KeyError:
+        nObjects = {}
+        objects = []
+    
+        for obj in channel:
+            if obj not in nObjects:
+                nObjects[obj] = 1
+            else:
+                nObjects[obj] += 1
+    
+        for obj, num in nObjects.iteritems():
+            if num == 1:
+                objects.append(obj)
+            else:
+                for i in range(num):
+                    objects.append(obj+str(i+1))
+        
+        objects.sort()
+
+        _zzhelpers_object_maps_[channel] = objects
+        return objects
--- Ntuplizer/python/helpers.py
+++ Ntuplizer/python/helpers.py
@@ -56,36 +56,5 @@ def combinePSets(ps, *more):
 
     return dict2PSet(params)
 
-_zzhelpers_object_maps_ = {}
-def mapObjects(channel):
-    '''
-    Return a list of objects of the form ['e1','e2','m1','m2'] or ['e1','e2','m']
-    Objects are in alphabetical/numerical order order
-    '''
-    global _zzhelpers_object_maps_
-
-    try:
-        return _zzhelpers_object_maps_[channel]
-    except KeyError:
-        nObjects = {}
-        objects = []
-    
-        for obj in channel:
-            if obj not in nObjects:
-                nObjects[obj] = 1
-            else:
-                nObjects[obj] += 1
-    
-        for obj, num in nObjects.iteritems():
-            if num == 1:
-                objects.append(obj)
-            else:
-                for i in range(num):
-                    objects.append(obj+str(i+1))
-        
-        objects.sort()
-
-        _zzhelpers_object_maps_[channel] = objects
-        return objects
 
 
