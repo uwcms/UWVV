331de66abea459a2da202a9efd2c4a00458eb281
Nate <nwoods@hep.wisc.edu>
2016-07-01 19:53:48 +0200

    Temporary fix so we can compile new checkouts while other packages are in transitional states.

--- recipe/setup.sh
+++ recipe/setup.sh
@@ -8,8 +8,12 @@ git clone https://github.com/Werbellin/RecoEgamma_8X RecoEgamma
 
 echo "Setting up ZZ matrix element stuff"
 git clone https://github.com/cms-analysis/HiggsAnalysis-ZZMatrixElement.git ZZMatrixElement
+
+# Get a version we know works during the awkward MELA v1->v2 transition
 pushd ZZMatrixElement
-git checkout -b from-master master
+git checkout c6d45de674ee145a8a4d74ba99f3e446dd1d9987
 popd
+
+git clone -b segfaultFix git@github.com:nwoods/KinZfitter.git
 
 popd
