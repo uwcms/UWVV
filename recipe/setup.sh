1b875319dba6b3bff8a0baff59d0403529fcaaaf
Nate <nwoods@hep.wisc.edu>
2016-07-10 18:02:45 +0200

    Allow recipe to be rerun.

--- recipe/setup.sh
+++ recipe/setup.sh
@@ -3,21 +3,29 @@
 
 pushd $CMSSW_BASE/src
 
-echo "Setting up electron ID code"
-git clone https://github.com/Werbellin/RecoEgamma_8X RecoEgamma
+if [ ! -d ./RecoEgamma ]; then
+    echo "Setting up electron ID code"
+    git clone https://github.com/Werbellin/RecoEgamma_8X RecoEgamma
+fi
 
-echo "Setting up ZZ matrix element stuff"
-git clone https://github.com/cms-analysis/HiggsAnalysis-ZZMatrixElement.git ZZMatrixElement
+if [ ! -d ZZMatrixElement ]; then
+    echo "Setting up ZZ matrix element stuff"
+    git clone https://github.com/cms-analysis/HiggsAnalysis-ZZMatrixElement.git ZZMatrixElement
 
-# Get a version we know works during the awkward MELA v1->v2 transition
-pushd ZZMatrixElement
-git checkout c6d45de674ee145a8a4d74ba99f3e446dd1d9987
-popd
+    # Get a version we know works during the awkward MELA v1->v2 transition
+    pushd ZZMatrixElement
+    git checkout c6d45de674ee145a8a4d74ba99f3e446dd1d9987
+    popd
+fi
 
-echo "Setting up Z kinematic fit stuff"
-git clone -b segfaultFix git@github.com:nwoods/KinZfitter.git
+if [ ! -d ./KinZfitter ]; then
+    echo "Setting up Z kinematic fit stuff"
+    git clone -b segfaultFix git@github.com:nwoods/KinZfitter.git
+fi
 
-echo "Setting up muon calibration"
-git clone https://github.com/bachtis/analysis.git -b KaMuCa_V3 KaMuCa
+if [ ! -d ./KaMuCa ]; then
+    echo "Setting up muon calibration"
+    git clone https://github.com/bachtis/analysis.git -b KaMuCa_V3 KaMuCa
+fi
 
 popd
