2d04cb3cc955058f7c9fb444759d1bd7f7d51210
Nate <nwoods@hep.wisc.edu>
2016-07-13 10:45:42 +0200

    Script to install and set up Python virtualenv with Rootpy and other nice things.

---
+++ recipe/setupPython.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+####    
+####    Set up a virtual environment with Rootpy, IPython, and Jupyter.
+####    Installs things the first time it runs, just activates the virtualenv
+####    thereafter.
+####
+####    Rootpy: Pythonic wrappers for PyROOT
+####    IPython: Nicer interactive Python (already in CMS software but broken)
+####    Jupyter: IPython notebooks
+####    
+####    Author: Nate Woods, U. Wisconsin
+####    
+
+
+if [ "$CMSSW_BASE" == "" ]; then
+    echo "Please do this only in a CMSSW environment. Did you cmsenv?"
+    exit 2
+fi
+
+if [ ! -d "$CMSSW_BASE"/src/UWVV/recipe ]; then
+    echo "UWVV doesn't look like it's set up in the usual way. Aborting."
+    exit 2
+fi
+
+export vpython="$CMSSW_BASE"/src/UWVV/recipe/vpython
+
+if [ ! -d "$vpython" ]; then
+    python "$CMSSW_BASE"/src/UWVV/recipe/virtualenv/virtualenv.py "$vpython"
+
+    source "$vpython"/bin/activate
+
+    pip install -U pip
+    pip install -U rootpy
+    pip install -U ipython
+    pip install -U jupyter
+else
+    source "$vpython"/bin/activate
+fi
+
+export PYTHONPATH="$vpython"/lib/python2.7/site-packages/:"$PYTHONPATH"
