f65724866d018f407eab987c02b093f573216e48
Nate <nwoods@hep.wisc.edu>
2016-07-13 19:38:39 +0200

    First version of config to prepare objects but save them in EDM format instead of making an ntuple.

---
+++ AnalysisTools/test/prepareLeptons_cfg.py
@@ -0,0 +1,180 @@
+import FWCore.ParameterSet.Config as cms
+import FWCore.ParameterSet.VarParsing as VarParsing
+import FWCore.PythonUtilities.LumiList as LumiList
+import FWCore.ParameterSet.Types as CfgTypes
+
+from UWVV.AnalysisTools.analysisFlowMaker import createFlow
+
+import os
+
+process = cms.Process("LEPTONS")
+
+options = VarParsing.VarParsing('analysis')
+
+options.inputFiles = '/store/mc/RunIIFall15MiniAODv2/GluGluHToZZTo4L_M2500_13TeV_powheg2_JHUgenV6_pythia8/MINIAODSIM/PU25nsData2015v1_76X_mcRun2_asymptotic_v12-v1/60000/02C0EC1D-F3E4-E511-ADCA-AC162DA603B4.root'
+options.outputFile = 'ntuplize.root'
+options.maxEvents = -1
+
+options.register('globalTag', "", 
+                 VarParsing.VarParsing.multiplicity.singleton,
+                 VarParsing.VarParsing.varType.string,
+                 "Global tag. If empty (default), auto tag is chosen based on isMC")
+options.register('isMC', 1,
+                 VarParsing.VarParsing.multiplicity.singleton,
+                 VarParsing.VarParsing.varType.int,
+                 "1 if simulation, 0 if data")
+options.register('eCalib', 0,
+                 VarParsing.VarParsing.multiplicity.singleton,
+                 VarParsing.VarParsing.varType.int,
+                 "1 if electron energy corrections are desired")
+options.register('muCalib', 0,
+                 VarParsing.VarParsing.multiplicity.singleton,
+                 VarParsing.VarParsing.varType.int,
+                 "1 if muon momentum corrections are desired")
+options.register('isSync', 0,
+                 VarParsing.VarParsing.multiplicity.singleton,
+                 VarParsing.VarParsing.varType.int,
+                 "1 if this is for synchronization purposes")
+options.register('skipEvents', 0,
+                 VarParsing.VarParsing.multiplicity.singleton,
+                 VarParsing.VarParsing.varType.int,
+                 "Number of events to skip (for debugging).")
+options.register('lumiMask', '',
+                 VarParsing.VarParsing.multiplicity.singleton,
+                 VarParsing.VarParsing.varType.string,
+                 "Lumi mask (for data only).")
+
+options.parseArguments()
+
+
+
+### To use IgProf's neat memory profiling tools, uncomment the following 
+### lines then run this cfg with igprof like so:
+###      $ igprof -d -mp -z -o igprof.mp.gz cmsRun ... 
+### this will create a memory profile every 250 events so you can track use
+### Turn the profile into text with
+###      $ igprof-analyse -d -v -g -r MEM_LIVE igprof.yourOutputFile.gz > igreport_live.res
+### To do a performance profile instead of a memory profile, change -mp to -pp
+### in the first command and remove  -r MEM_LIVE from the second
+### For interpretation of the output, see http://igprof.org/text-output-format.html
+# from IgTools.IgProf.IgProfTrigger import igprof
+# process.load("IgTools.IgProf.IgProfTrigger")
+# process.igprofPath = cms.Path(process.igprof)
+# process.igprof.reportEventInterval     = cms.untracked.int32(250)
+# process.igprof.reportToFileAtBeginJob  = cms.untracked.string("|gzip -c>igprof.begin-job.gz")
+# process.igprof.reportToFileAtEvent = cms.untracked.string("|gzip -c>igprof.%I.%E.%L.%R.event.gz")
+
+
+# Basic stuff for all jobs
+
+process.load("Configuration.StandardSequences.GeometryRecoDB_cff")
+
+process.load("Configuration.StandardSequences.FrontierConditions_GlobalTag_cff")
+from Configuration.AlCa.GlobalTag import GlobalTag
+
+if options.globalTag:
+    gt = options.globalTag
+elif options.isMC:
+    gt = 'auto:run2_mc'
+else:
+    gt = 'auto:run2_data'
+process.GlobalTag = GlobalTag(process.GlobalTag, gt)
+
+process.load("FWCore.MessageLogger.MessageLogger_cfi")
+process.schedule = cms.Schedule()
+
+process.MessageLogger.cerr.FwkReport.reportEvery = 100
+
+process.source = cms.Source(
+    "PoolSource",
+    fileNames = cms.untracked.vstring(options.inputFiles),
+    skipEvents = cms.untracked.uint32(options.skipEvents),
+    )
+
+if options.lumiMask:
+    lumiJSON = options.lumiMask
+    if not os.path.exists(lumiJSON):
+        raise IOError("Lumi mask file {} not found.".format(lumiJSON))
+    lumiList = LumiList.LumiList(filename = lumiJSON)
+    runs = lumiList.getRuns()
+    lumisToProcess = CfgTypes.untracked(CfgTypes.VLuminosityBlockRange())
+    lumisToProcess.extend(lumiList.getCMSSWString().split(','))
+    process.source.lumisToProcess = lumisToProcess
+
+process.maxEvents = cms.untracked.PSet(
+    input=cms.untracked.int32(options.maxEvents)
+    )
+
+#############################################################################
+#    Make the analysis flow. It is assembled from a list of classes, each   #
+#    of which adds related steps to the sequence.                           #
+#############################################################################
+FlowSteps = []
+
+# everybody needs vertex cleaning
+from UWVV.AnalysisTools.templates.VertexCleaning import VertexCleaning
+FlowSteps.append(VertexCleaning)
+
+# everybody needs basic lepton stuff
+from UWVV.AnalysisTools.templates.ElectronBaseFlow import ElectronBaseFlow
+FlowSteps.append(ElectronBaseFlow)
+from UWVV.AnalysisTools.templates.RecomputeElectronID import RecomputeElectronID
+FlowSteps.append(RecomputeElectronID)
+
+from UWVV.AnalysisTools.templates.MuonBaseFlow import MuonBaseFlow
+FlowSteps.append(MuonBaseFlow)
+
+from UWVV.AnalysisTools.templates.MuonScaleFactors import MuonScaleFactors
+FlowSteps.append(MuonScaleFactors)
+
+# jet energy corrections and basic preselection
+from UWVV.AnalysisTools.templates.JetBaseFlow import JetBaseFlow
+FlowSteps.append(JetBaseFlow)
+if options.isMC:
+    from UWVV.AnalysisTools.templates.JetEnergySmearing import JetEnergySmearing
+    FlowSteps.append(JetEnergySmearing)
+
+
+# Lepton calibrations
+if options.eCalib:
+    from UWVV.AnalysisTools.templates.ElectronCalibration import ElectronCalibration
+    FlowSteps.append(ElectronCalibration)
+
+if options.muCalib:
+    from UWVV.AnalysisTools.templates.MuonCalibration import MuonCalibration
+    FlowSteps.append(MuonCalibration)
+
+# ZZ stuff
+from UWVV.AnalysisTools.templates.ZZFlow import ZZFlow
+FlowSteps.append(ZZFlow)
+
+# but not actually the Z/ZZ combinations
+from UWVV.AnalysisTools.templates.NoCombinedStates import NoCombinedStates
+FlowSteps.append(NoCombinedStates)
+
+# Turn all these into a single flow class
+FlowClass = createFlow(*FlowSteps)
+flow = FlowClass('flow', process, 
+                 isMC=bool(options.isMC), isSync=bool(options.isSync))
+
+outputCommands = cms.untracked.vstring(
+    'drop *',
+    'keep *_packedGenParticles_*_*',
+    'keep *_prunedGenParticles_*_*',
+    'keep *_packedPFCandidates_*_*',
+    )
+for obj, objName in flow.finalTags().iteritems():
+    print obj, objName
+    outputCommands.append('keep *_{}_*_*'.format(objName.split(':')[0]))
+
+process.out = cms.OutputModule(
+    "PoolOutputModule",
+    fileName = cms.untracked.string(options.outputFile),
+    dropMetaData = cms.untracked.string("ALL"),
+    outputCommands = outputCommands,
+    )
+
+process.save = cms.EndPath(process.out)
+
+process.schedule.append(flow.getPath())
+process.schedule.append(process.save)
