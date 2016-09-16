c517840d82bd5e42138697f232da1e0c6d16845c
Kenneth Long <kdlong@hep.wisc.edu>
2016-09-16 17:09:10 +0200

    Adding B-jet counters
    Currently PATObjectCounting works only in series, i.e. the collection is
    loaded and counter subject to exactly one selection each time. This
    isn't very efficient.

--- Ntuplizer/python/templates/countBranches.py
+++ Ntuplizer/python/templates/countBranches.py
@@ -13,6 +13,24 @@ wzCountBranches = cms.PSet(
                                 'userInt("nTightMuon") : 999'),
         nMediumMuonICHEP = cms.string('? hasUserInt("nMediumMuonICHEP") ? '
                                       'userInt("nMediumMuonICHEP") : 999'),
+        nPassJPL = cms.string('? hasUserInt("nPassJPL") ? '
+                              'userInt("nPassJPL") : 999'),
+        nPassJPM = cms.string('? hasUserInt("nPassJPM") ? ' 
+                              'userInt("nPassJPM") : -999'),
+        nPassJPT = cms.string('? hasUserInt("nPassJPT") ? '
+                              'userInt("nPassJPT") : -999'),
+        nPassCSVv2L = cms.string('? hasUserInt("nPassCSVv2L") ? '
+                              'userInt("nPassCSVv2L") : -999'),
+        nPassCSVv2M = cms.string('? hasUserInt("nPassCSVv2M") ? '
+                                 'userInt("nPassCSVv2M") : -999'),
+        nPassCSVv2T = cms.string('? hasUserInt("nPassCSVv2T") ? '
+                                 'userInt("nPassCSVv2T") : -999'),
+        nPassCMVAv2L = cms.string('? hasUserInt("nPassCMVAv2L") ? '
+                                  'userInt("nPassCMVAv2L") : -999'),
+        nPassCMVAv2M = cms.string('? hasUserInt("nPassCMVAv2M") ? '
+                                  'userInt("nPassCMVAv2M") : -999'),
+        nPassCMVAv2T = cms.string('? hasUserInt("nPassCMVAv2T") ? '
+                                  'userInt("nPassCMVAv2T") : -999'),
         ),
     )
 
