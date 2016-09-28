3f249f53184f2d78ae9c3ada6ef6399dc7be056b
Nate <nwoods@hep.wisc.edu>
2016-09-28 18:31:32 +0200

    Correct scale factors and errors for gap electrons (working around problem with multiple ternary operators in one StringObjectFunction).

--- Ntuplizer/python/templates/electronBranches.py
+++ Ntuplizer/python/templates/electronBranches.py
@@ -20,16 +20,27 @@ electronBranches = cms.PSet(
         SCEnergy = cms.string('superCluster.energy'),
         SCRawEnergy = cms.string('superCluster.rawEnergy'),
 
-        EffScaleFactor = cms.string('? isGap ? '
-                                    '(? hasUserFloat("effScaleFactorGap") ?'
-                                    ' userFloat("effScaleFactorGap") : 1.) :'
-                                    '(? hasUserFloat("effScaleFactor") ? '
-                                    'userFloat("effScaleFactor") : 1.)'),
-        EffScaleFactorError = cms.string('? isGap ? '
-                                         '(? hasUserFloat("effScaleFactorErrorGap") ?'
-                                         ' userFloat("effScaleFactorErrorGap") : 0.) :'
-                                         '(? hasUserFloat("effScaleFactorError") ? '
-                                         'userFloat("effScaleFactorError") : 0.)'),
+        # apparently there's a bug with multiple ternary operators, so cheat
+        EffScaleFactor = cms.string('? hasUserFloat("effScaleFactorGap") && '
+                                    '  hasUserFloat("effScaleFactor") ? '
+                                    'isGap * userFloat("effScaleFactorGap") + '
+                                    '(1-isGap) * userFloat("effScaleFactor") :'
+                                    '1.'),
+        # EffScaleFactor = cms.string('? isGap ? '
+        #                             '(? hasUserFloat("effScaleFactorGap") ?'
+        #                             ' userFloat("effScaleFactorGap") : 1.) :'
+        #                             '(? hasUserFloat("effScaleFactor") ? '
+        #                             'userFloat("effScaleFactor") : 1.)'),
+        EffScaleFactorError = cms.string('? hasUserFloat("effScaleFactorErrorGap") && '
+                                         '  hasUserFloat("effScaleFactorError") ? '
+                                         'isGap * userFloat("effScaleFactorErrorGap") + '
+                                         '(1-isGap) * userFloat("effScaleFactorError") :'
+                                         '0.'),
+        # EffScaleFactorError = cms.string('? isGap ? '
+        #                                  '(? hasUserFloat("effScaleFactorErrorGap") ?'
+        #                                  ' 4. : 0.) :' #' userFloat("effScaleFactorErrorGap") : 0.) :'
+        #                                  '(? hasUserFloat("effScaleFactorError") ? '
+        #                                  ' 4. : 0.)'), #'userFloat("effScaleFactorError") : 0.)'),
         TrkRecoEffScaleFactor = cms.string('? hasUserFloat("trkRecoEffScaleFactor") ? '
                                            'userFloat("trkRecoEffScaleFactor") : 1.'),
         MtToMET = cms.string('mtToMET'),
