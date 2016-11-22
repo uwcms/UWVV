f57ae43bded6ba6e458c44938a936bedf2d24bf5
Kenneth Long <kdlong@hep.wisc.edu>
2016-11-22 11:14:31 +0100

    Updating submit scripts and adding MC

--- scripts/WZSubmission/makeSubmit_Data.sh
+++ scripts/WZSubmission/makeSubmit_Data.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 DATE=`date +%Y-%m-%d`
-echo $DATE
 if [ -z "$CMSSW_BASE" ]; then
     echo "Must submit within a CMS environment"
     exit 1
@@ -8,7 +7,7 @@ fi
 
 uwvv=$CMSSW_BASE/src/UWVV
 ${uwvv}/Utilities/scripts/submitJobs.py \
-    -o ${uwvv}/submit_scripts/submitWZData_Run2016B-PromptReco-v2_$DATE.sh \
+    -o ${uwvv}/scripts/WZSubmission/GeneratedScripts/submitWZData_Run2016B-PromptReco-v2_$DATE.sh \
     --dataEra \
         Run2016B-PromptReco-v2 \
     --samples \
@@ -25,7 +24,7 @@ ${uwvv}/Utilities/scripts/submitJobs.py \
         globalTag=80X_dataRun2_Prompt_ICHEP16JEC_v0
 uwvv=$CMSSW_BASE/src/UWVV
 ${uwvv}/Utilities/scripts/submitJobs.py \
-    -o ${uwvv}/submit_scripts/submitWZData_Run2016C-PromptReco-v2_$DATE.sh \
+    -o ${uwvv}/scripts/WZSubmission/GeneratedScripts/submitWZData_Run2016C-PromptReco-v2_$DATE.sh \
     --dataEra \
         Run2016C-PromptReco-v2 \
     --samples \
@@ -41,7 +40,7 @@ ${uwvv}/Utilities/scripts/submitJobs.py \
         muCalib=1 \
         globalTag=80X_dataRun2_Prompt_ICHEP16JEC_v0
 ${uwvv}/Utilities/scripts/submitJobs.py \
-    -o ${uwvv}/submit_scripts/submitWZData_Run2016D-PromptReco-v2_$DATE.sh \
+    -o ${uwvv}/scripts/WZSubmission/GeneratedScripts/submitWZData_Run2016D-PromptReco-v2_$DATE.sh \
     --dataEra \
         Run2016D-PromptReco-v2 \
     --samples \
@@ -56,9 +55,9 @@ ${uwvv}/Utilities/scripts/submitJobs.py \
         eCalib=1 
         muCalib=1 \
         globalTag=80X_dataRun2_Prompt_ICHEP16JEC_v0
-chmod +x $CMSSW_BASE/src/UWVV/submit_scripts/submitWZData_Run2016B-PromptReco-v2_$DATE.sh
-$CMSSW_BASE/src/UWVV/submit_scripts/submitWZData_Run2016B-PromptReco-v2_$DATE.sh
-chmod +x $CMSSW_BASE/src/UWVV/submit_scripts/submitWZData_Run2016C-PromptReco-v2_$DATE.sh
-$CMSSW_BASE/src/UWVV/submit_scripts/submitWZData_Run2016C-PromptReco-v2_$DATE.sh
-chmod +x $CMSSW_BASE/src/UWVV/submit_scripts/submitWZData_Run2016D-PromptReco-v2_$DATE.sh
-$CMSSW_BASE/src/UWVV/submit_scripts/submitWZData_Run2016D-PromptReco-v2_$DATE.sh
+chmod +x $CMSSW_BASE/src/UWVV/scripts/WZSubmission/GeneratedScripts/submitWZData_Run2016B-PromptReco-v2_$DATE.sh
+$CMSSW_BASE/src/UWVV/scripts/WZSubmission/GeneratedScripts/submitWZData_Run2016B-PromptReco-v2_$DATE.sh
+chmod +x $CMSSW_BASE/src/UWVV/scripts/WZSubmission/GeneratedScripts/submitWZData_Run2016C-PromptReco-v2_$DATE.sh
+$CMSSW_BASE/src/UWVV/scripts/WZSubmission/GeneratedScripts/submitWZData_Run2016C-PromptReco-v2_$DATE.sh
+chmod +x $CMSSW_BASE/src/UWVV/scripts/WZSubmission/GeneratedScripts/submitWZData_Run2016D-PromptReco-v2_$DATE.sh
+$CMSSW_BASE/src/UWVV/scripts/WZSubmission/GeneratedScripts/submitWZData_Run2016D-PromptReco-v2_$DATE.sh
