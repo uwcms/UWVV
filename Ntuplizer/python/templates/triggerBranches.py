84eef07b499d9917a703acb53a8e06c5e8326558
Kenneth Long <kdlong@hep.wisc.edu>
2017-02-28 16:57:27 +0100

    Schedule MET filters for data and MC

--- Ntuplizer/python/templates/triggerBranches.py
+++ Ntuplizer/python/templates/triggerBranches.py
@@ -118,12 +118,53 @@ triggerBranches_2016H = cms.PSet(
     checkPrescale = cms.bool(False),
     )
 
-badMuonFilter = cms.PSet(
-    trigNames = cms.vstring('badMuons', 'duplicateMuons'),
-    badMuonsPaths = cms.vstring('Flag_badMuons'),
-    duplicateMuonsPaths = cms.vstring('Flag_duplicateMuons'),
+# Bad Muon filter for ReMiniAOD following: https://twiki.cern.ch/twiki/bin/view/CMSPublic/ReMiniAOD03Feb2017Notes
+# MET filters for Moriond 2017 following: https://twiki.cern.ch/twiki/bin/viewauth/CMS/MissingETOptionalFiltersRun2
+metAndBadMuonFilters = cms.PSet(
+    trigNames = cms.vstring(
+        'Flag_HBHENoiseFilter', 
+        'Flag_HBHENoiseIsoFilter',
+        'Flag_EcalDeadCellTriggerPrimitiveFilter',
+        'Flag_goodVertices',
+        'Flag_eeBadScFilter',
+        'Flag_globalTightHalo2016Filter',
+        'Flag_badMuons', 
+        'Flag_duplicateMuons',
+    ),
+    Flag_HBHENoiseFilterPaths = cms.vstring('Flag_HBHENoiseFilter'),
+    Flag_HBHENoiseIsoFilterPaths = cms.vstring('Flag_HBHENoiseIsoFilter'),
+    Flag_EcalDeadCellTriggerPrimitiveFilterPaths = cms.vstring('Flag_EcalDeadCellTriggerPrimitiveFilter'),
+    Flag_goodVerticesPaths = cms.vstring('Flag_goodVertices'),
+    Flag_eeBadScFilterPaths = cms.vstring('Flag_eeBadScFilter'),
+    Flag_globalTightHalo2016FilterPaths = cms.vstring('Flag_globalTightHalo2016Filter'),
+    Flag_BadChargedCandidateFilterPaths = cms.vstring('Flag_BadChargedCandidateFilter'),
+    Flag_BadPFMuonFilterPaths = cms.vstring('Flag_BadPFMuonFilter'),
+    
+    Flag_badMuonsPaths = cms.vstring('Flag_badMuons'),
+    Flag_duplicateMuonsPaths = cms.vstring('Flag_duplicateMuons'),
     trigResultsSrc = cms.InputTag("TriggerResults", "", "PAT"),
     trigPrescaleSrc = cms.InputTag("patTrigger"),
     checkPrescale = cms.bool(False),
     )
 
+# MET filters for Moriond 2017 following: https://twiki.cern.ch/twiki/bin/viewauth/CMS/MissingETOptionalFiltersRun2
+metFilters = cms.PSet(
+    trigNames = cms.vstring(
+        'Flag_HBHENoiseFilter', 
+        'Flag_HBHENoiseIsoFilter',
+        'Flag_EcalDeadCellTriggerPrimitiveFilter',
+        'Flag_goodVertices',
+        'Flag_eeBadScFilter',
+        'Flag_globalTightHalo2016Filter',
+    ),
+    Flag_HBHENoiseFilterPaths = cms.vstring('Flag_HBHENoiseFilter'),
+    Flag_HBHENoiseIsoFilterPaths = cms.vstring('Flag_HBHENoiseIsoFilter'),
+    Flag_EcalDeadCellTriggerPrimitiveFilterPaths = cms.vstring('Flag_EcalDeadCellTriggerPrimitiveFilter'),
+    Flag_goodVerticesPaths = cms.vstring('Flag_goodVertices'),
+    Flag_eeBadScFilterPaths = cms.vstring('Flag_eeBadScFilter'),
+    Flag_globalTightHalo2016FilterPaths = cms.vstring('Flag_globalTightHalo2016Filter'),
+    
+    trigResultsSrc = cms.InputTag("TriggerResults", "", "PAT"),
+    trigPrescaleSrc = cms.InputTag("patTrigger"),
+    checkPrescale = cms.bool(False),
+    )
