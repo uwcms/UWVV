c493f67e9100026ab29e3fde13ff75d2de7676ef
Kenneth Long <kdlong@hep.wisc.edu>
2017-03-01 00:44:10 +0100

    Add dataset name to metaTree

--- scripts/test/testWZ_DoubleMuData.sh
+++ scripts/test/testWZ_DoubleMuData.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 cmsRun UWVV/Ntuplizer/test/ntuplize_cfg.py \
     inputFiles=/store/data/Run2016C/DoubleMuon/MINIAOD/03Feb2017-v1/110000/02292378-59ED-E611-BA0B-0CC47A4D768E.root \
+    datasetName=/DoubleMuon/Run2016C-03Feb2017-v1/MINIAOD \
     outputFile=test_DoubleMu.root \
     channels=wz \
     isMC=0 \
