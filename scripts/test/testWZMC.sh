69a226827cf3ed0f5a654a8bbcb3f2b7043ef310
Kenneth Long <kdlong@hep.wisc.edu>
2017-03-27 17:07:04 +0200

    Update WZ submit scripts

--- scripts/test/testWZMC.sh
+++ scripts/test/testWZMC.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-cmsRun UWVV/Ntuplizer/test/ntuplize_cfg.py \
+igprof -d -mp -z -o igprof.mp.gz cmsRun UWVV/Ntuplizer/test/ntuplize_cfg.py \
     inputFiles=/store/mc/RunIISummer16MiniAODv2/WZTo3LNu_3Jets_MLL-50_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/MINIAODSIM/PUMoriond17_80X_mcRun2_asymptotic_2016_TrancheIV_v6-v1/100000/14CC20E6-A1CF-E611-8950-0025905A6094.root \
     datasetName=/WZTo3LNu_3Jets_MLL-50_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/RunIISummer16MiniAODv2-PUMoriond17_80X_mcRun2_asymptotic_2016_TrancheIV_v6-v1/MINIAODSIM \
     outputFile=testMC.root \
