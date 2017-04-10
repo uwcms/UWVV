67c8ec7a6e43d9010a90b87a927827da5c0abda2
Nate <nwoods@hep.wisc.edu>
2017-04-10 11:31:54 +0200

    Output dressed gen particle collection as vector<DressedGenParticle> instead of reco::CandidateCollection.

--- AnalysisTools/plugins/DressedGenParticlesProducer.cc
+++ AnalysisTools/plugins/DressedGenParticlesProducer.cc
@@ -30,15 +30,15 @@ DressedGenParticlesProducer::DressedGenParticlesProducer(
         associatesToken_(consumes<reco::GenParticleCollection>(
             cfg.getParameter<edm::InputTag>("associates"))),
         dRmax_(cfg.getUntrackedParameter<double>("dRmax", 0.1)) {
-    produces<reco::CandidateCollection>();
+    produces<std::vector<DressedGenParticle> >();
 }
 
 void DressedGenParticlesProducer::produce(edm::Event& event, const edm::EventSetup& es) {
-    std::auto_ptr<reco::CandidateCollection> dressedCollection(new reco::CandidateCollection);
-    
+    std::auto_ptr<std::vector<DressedGenParticle> > dressedCollection(new std::vector<DressedGenParticle>);
+
     edm::Handle<reco::GenParticleCollection> baseCollection;
     event.getByToken(baseCollectionToken_, baseCollection);
-    
+
     edm::Handle<reco::GenParticleCollection> associates;
     event.getByToken(associatesToken_, associates);
 
@@ -47,9 +47,9 @@ void DressedGenParticlesProducer::produce(edm::Event& event, const edm::EventSet
             *associates, dRmax_);
         dressedCollection->push_back(dressed_part);
     }
-    //std::cout << "All associates were unique? " << allUniqueAssociates(*dressedCollection) 
+    //std::cout << "All associates were unique? " << allUniqueAssociates(*dressedCollection)
     //          << std::endl;
-    //std::sort(dressedCollection->begin(),dressedCollection->end(), 
+    //std::sort(dressedCollection->begin(),dressedCollection->end(),
     //    [](const reco::Candidate& part1, const reco::Candidate& part2)
     //        {return part1.pt() > part2.pt();});
         //GreaterByPt<reco::Candidate>());
@@ -60,20 +60,20 @@ bool DressedGenParticlesProducer::allUniqueAssociates(
         reco::CandidateCollection& dressedParticles) {
     std::vector<reco::GenParticle> allAssociated;
     for (const auto& particle : dressedParticles) {
-        const DressedGenParticle& dressed_part = 
+        const DressedGenParticle& dressed_part =
             dynamic_cast<const DressedGenParticle&>(particle);
         reco::GenParticleCollection associated = dressed_part.getAssociated();
-        allAssociated.insert(allAssociated.end(), 
+        allAssociated.insert(allAssociated.end(),
             associated.begin(), associated.end());
     }
-    std::set<reco::GenParticle, GreaterByPt<reco::Candidate>> 
+    std::set<reco::GenParticle, GreaterByPt<reco::Candidate>>
         uniqueAssociated(allAssociated.begin(), allAssociated.end());
     if (allAssociated.size() != uniqueAssociated.size()) {
         std::cout << "----------------------------" << std::endl;
         std::cout << "Photons for events with duplicates!" << std::endl;
         for (const auto& assoc : allAssociated) {
-            std::cout << " pdgId " << assoc.pdgId() << " pt " 
-                      << assoc.pt() << " eta " << assoc.eta() << std::endl; 
+            std::cout << " pdgId " << assoc.pdgId() << " pt "
+                      << assoc.pt() << " eta " << assoc.eta() << std::endl;
         }
         removeDoubleCounting(dressedParticles);
     }
@@ -83,30 +83,30 @@ void DressedGenParticlesProducer::removeDoubleCounting(
         reco::CandidateCollection& dressedParticles) {
     for(size_t i = 0; i < dressedParticles.size(); i++)
     {
-        const DressedGenParticle& dressed_part_i = 
+        const DressedGenParticle& dressed_part_i =
             dynamic_cast<const DressedGenParticle&>(dressedParticles[i]);
         reco::GenParticleCollection associates = dressed_part_i.getAssociated();
         for(size_t j = i + 1; j < dressedParticles.size(); j++) {
-            const DressedGenParticle& dressed_part_j = 
+            const DressedGenParticle& dressed_part_j =
                 dynamic_cast<const DressedGenParticle&>(dressedParticles[j]);
             std::cout << std::endl << "Lepton # " << j;
             std::cout << std::endl << "Associated Photons = " << dressed_part_j.numAssociated();
             std::cout << std::endl << "Lepton kinematics:";
-            std::cout << std::endl << " pdgId " << dressed_part_j.pdgId() << " pt " 
-                        << dressed_part_j.pt() << " eta " << dressed_part_j.eta() << std::endl; 
+            std::cout << std::endl << " pdgId " << dressed_part_j.pdgId() << " pt "
+                        << dressed_part_j.pt() << " eta " << dressed_part_j.eta() << std::endl;
             for (const auto& assoc : associates) {
                 std::cout << "Associated = " << dressed_part_j.isAssociated(assoc);
-                std::cout << std::endl << " pdgId " << assoc.pdgId() << " pt " 
-                        << assoc.pt() << " eta " << assoc.eta() << std::endl; 
+                std::cout << std::endl << " pdgId " << assoc.pdgId() << " pt "
+                        << assoc.pt() << " eta " << assoc.eta() << std::endl;
             }
             //auto overlap = std::set_intersection(associates,
             //    dressedParticles[j].getAssociated());
-            //disambiguateAssociates(overlap, 
+            //disambiguateAssociates(overlap,
             //    dressedParticles[i], dressedParticles[j]);
         }
     }
 }
-//void DressedGenParticlesProducer::disambiguateAssociatesByDR(auto overlap, 
+//void DressedGenParticlesProducer::disambiguateAssociatesByDR(auto overlap,
 //        reco::GenParticleCollection dressed1,
 //        reco::GenParticleCollection dressed2) {
 //    for (auto cand : overlap) {
