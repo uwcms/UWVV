e248c9cdc17123b09f1aeb2f61fb269c6cee48b3
Kenneth Long <kdlong@hep.wisc.edu>
2017-08-07 13:17:20 +0200

    Now works for nJets, but jes/jer variables are not defined...
    Fixing bug(s) in CleanedJetCollectionEmbedder and setting things up so
    the collection can be embedded in the event.

--- DataFormats/src/classes.h
+++ DataFormats/src/classes.h
@@ -1,6 +1,8 @@
 #include "UWVV/DataFormats/interface/DressedGenParticleFwd.h"
 #include "UWVV/DataFormats/interface/DressedGenParticle.h"
 
+#include "DataFormats/PatCandidates/interface/Jet.h"
+
 namespace {
     struct UWVV_DataFormats_dicts {
         DressedGenParticleCollection p1;
@@ -12,5 +14,8 @@ namespace {
             edm::ClonePolicy<DressedGenParticle> > dummyOV1;
         edm::OwnVector<DressedGenParticle,
             edm::ClonePolicy<DressedGenParticle> >::const_iterator dummyit; 
+
+        edm::PtrVector<pat::Jet> dummyPtrVectorPatJet;
+        pat::UserHolder<edm::PtrVector<pat::Jet>> dummyPtrUserHolderPtrVectorPatJet; 
     };
 }
